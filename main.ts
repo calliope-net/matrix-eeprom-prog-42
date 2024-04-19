f18572937462d61a2d881daa6c6d1efded1751df
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-04-19 16:21:13 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,30 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    matrix.comment("alle Zeichen aus Programmspeicher anzeigen")
+    matrix.clearMatrix()
+    matrix.writeCharset()
+    matrix.displayMatrix()
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    matrix.comment("alle Zeichen aus Programmspeicher in EEPROM F900-FFFF programmieren")
+    matrix.clearMatrix()
+    matrix.writeCharset()
+    if (matrix.burnEEPROM("F800", 2, 15, 128, 63488)) {
+        basic.setLedColor(0x00ff00)
+    } else {
+        basic.setLedColor(0xff0000)
+    }
+    matrix.writeCharsetEEPROM()
+    matrix.displayMatrix()
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    matrix.comment("EEPROM F800-FFFF leer, wenn 128 Byte mit Wert 255 gezählt werden")
+    matrix.clearMatrix()
+    for (let Index = 0; Index <= 15; Index++) {
+        adr = matrix.matrix_eEEPROM_Startadresse(matrix.eEEPROM_Startadresse.F800) + Index * 128
+        matrix.writeTextCharset(Index, 0, matrix.matrix_text("" + matrix.formatHex(adr, NumberFormat.UInt16BE) + " " + matrix.checkEEPROM(adr, 255)))
+    }
+    matrix.displayMatrix()
+})
+let adr = 0
+matrix.init(matrix.ePages.y128)
+matrix.displayMatrix()
